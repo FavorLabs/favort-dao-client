1c9fba416699bc7550feeb0235e4237395ca3f94
CharlotteFl <CharlotteFl@favorlabs.io>
2023-03-11 18:13:13 +0800

    fix: fix the news like logic

--- src/components/GraphicMessage/index.tsx
+++ src/components/GraphicMessage/index.tsx
@@ -6,25 +6,57 @@ import CommunityInfo from '@/components/CommunityInfo';
 import CommentArea from '@/components/CommentArea';
 import { PostInfo } from '@/declare/tubeApiType';
 import { getContent } from '@/utils/util';
-import { useResourceUrl } from '@/utils/hooks';
+import { useResourceUrl, useUrl } from '@/utils/hooks';
 import moreImg from '@/assets/img/more-img.png';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import PopupContent from '@/components/PopupContent';
+import PostApi from '@/services/tube/PostApi';
 
 export type Props = {
   post: PostInfo;
 };
 
 const GraphicMessage: React.FC<Props> = (props) => {
-  const { dao, contents, view_count, upvote_count, comment_count, created_on } =
-    props.post;
+  const {
+    dao,
+    contents,
+    view_count,
+    upvote_count,
+    comment_count,
+    created_on,
+    id,
+  } = props.post;
   if (!dao) return <></>;
+  const url = useUrl();
   const [visible, setVisible] = useState(false);
+  const [like, setLike] = useState<boolean>(false);
+  const [likeCount, setLikeCount] = useState<number>(upvote_count);
   const moreClick = () => {
     setVisible(!visible);
   };
   const imagesResUrl = useResourceUrl('images');
   const info = getContent(contents);
+
+  const getPostLikeStatus = async () => {
+    const { data } = await PostApi.checkPostLike(url, id);
+    if (data.data) {
+      setLike(data.data.status);
+    }
+  };
+
+  const postLike = async () => {
+    const { data } = await PostApi.postLike(url, id);
+    if (data.data) {
+      setLike(data.data.status);
+      if (data.data.status) setLikeCount(likeCount + 1);
+      else setLikeCount(likeCount - 1);
+    }
+  };
+
+  useEffect(() => {
+    if (id) getPostLikeStatus();
+  }, [id]);
+
   return (
     <Space className={styles.container} direction="vertical">
       <div className={styles.inContent}>
@@ -54,7 +86,9 @@ const GraphicMessage: React.FC<Props> = (props) => {
         <CommentArea
           watchNum={view_count}
           commentOnNum={comment_count}
-          likeNum={upvote_count}
+          likeNum={likeCount}
+          likeStatus={like}
+          likeHandle={postLike}
         />
       </div>
       <Popup
