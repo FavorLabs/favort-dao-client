c8880d96ae5c35c1521929aa930d8d6f95cc2c3d
samuelflabs <samuel@favorlabs.io>
2023-03-14 18:02:41 +0800

    fix: modify the route of the community group chat

--- src/components/MyAttention/index.tsx
+++ src/components/MyAttention/index.tsx
@@ -114,13 +114,6 @@ const MyAttention: React.FC<Props> = (props) => {
             ) : (
               <></>
             )}
-            <div>aaa</div>
-            <div>aaa</div>
-            <div>aaa</div>
-            <div>aaa</div>
-            <div>aaa</div>
-            <div>aaa</div>
-            <div>aaa</div>
           </div>
         </div>
       </div>
